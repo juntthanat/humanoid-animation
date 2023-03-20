aa4ccb65f31ed6b1f7a8ad60321f7b727fd90069
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2023-03-20 19:55:08 +0700

    feat: update test
    add:
    - mannequin model
    - room model
    remove
    - lever
    - lever animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,55 @@
 import { useState, useEffect} from "react";
 import "./App.css";
 import "aframe"
-import Three_animation from "./three/three_animation";
+// import Three_animation from "./three/three_animation";
 
 function App() {
-  useEffect(() => {
-    var lever = document.querySelector("#lever");
-    var handle = true;
-    lever.addEventListener('click', function(e){
-      if (handle == true){
-        lever.setAttribute('animation', {
-          property: 'rotation',
-          from: '45 0 90',
-          to: {x, y, z},
-          dur: '2500',
-          pauseEvents: 'mouseleave',
-          resumeEvents: 'mouseenter'
-        })
-        handle = false;
-      } else {
-        lever.setAttribute('animation', {
-          property: 'rotation',
-          from: '-45 0 90',
-          to: '45 0 90',
-          dur: '2500',
-          pauseEvents: 'mouseleave',
-          resumeEvents: 'mouseenter'
-        })
-        handle = true;
-      }
+  // useEffect(() => {
+  //   var lever = document.querySelector("#lever");
+  //   var handle = true;
+  //   lever.addEventListener('click', function(e){
+  //     if (handle == true){
+  //       lever.setAttribute('animation', {
+  //         property: 'rotation',
+  //         from: '45 0 90',
+  //         to: {x, y, z},
+  //         dur: '2500',
+  //         pauseEvents: 'mouseleave',
+  //         resumeEvents: 'mouseenter'
+  //       })
+  //       handle = false;
+  //     } else {
+  //       lever.setAttribute('animation', {
+  //         property: 'rotation',
+  //         from: '-45 0 90',
+  //         to: '45 0 90',
+  //         dur: '2500',
+  //         pauseEvents: 'mouseleave',
+  //         resumeEvents: 'mouseenter'
+  //       })
+  //       handle = true;
+  //     }
       
-    });
-  });
+  //   });
+  // });
 
   var x = -45;
   var y = 0;
   var z = 90;
 
+
+
   return (
     <div className="App">
 
       <a-scene >
-      <a-entity id="mouseCursor" cursor="rayOrigin: mouse"></a-entity>
+        <a-assets>
+          <a-asset-item src="src\assets\room_704.gltf" id="room"></a-asset-item>
+          <a-asset-item src="src\assets\mannequin_female.gltf" id="avatar"></a-asset-item>
+        </a-assets>
+        <a-gltf-model src="#room"></a-gltf-model>
+        <a-gltf-model src="#avatar" position="3 0 0"></a-gltf-model>
+      {/* <a-entity id="mouseCursor" cursor="rayOrigin: mouse"></a-entity>
       <a-entity light="type:directional; castShadow:true;" position="1 1 1"></a-entity>
         <a-entity 
           geometry="primitive:box; height:0.5; width:2; depth:2"
@@ -69,7 +77,8 @@ function App() {
           animation__mouseenter="property: components.material.material.color; type: color; to: blue; startEvents: mouseenter; dur: 500"
           animation__mouseleave="property: components.material.material.color; type: color; to: red; startEvents: mouseleave; dur: 500"
           ></a-entity>
-        </a-entity>
+        </a-entity> */}
+        
       </a-scene>
     </div>
   );
