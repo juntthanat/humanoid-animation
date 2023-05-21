2cf108527b2bdf8788c044bef5839d7b43832333
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2023-05-21 12:02:29 +0700

    Update: Remove Change Avatar Add Rotation Balance

--- src/Page/App.jsx
+++ src/Page/App.jsx
@@ -32,11 +32,15 @@ function App() {
   const [rightLeg, setRightLeg] = useState();
   const [rightUpLeg, setRightUpLeg] = useState();
 
+  const [body, setBody] = useState();
+
   // Check if Model is rendered
   useEffect(() => {
     let interval;
     interval = setInterval(() => {
       let model = document.getElementById("model").object3D;
+      // let model = _model.object3D;
+      // console.log(model)
       if (model) {
         clearInterval(interval);
         setHead(
@@ -70,6 +74,7 @@ function App() {
         setRightLeg(
           model?.children[0]?.children[0]?.children[0]?.children[2]?.children[0]
         );
+        setBody(model?.children[0]?.children[0]?.children[0]);
         modelRef.current = model;
         console.log("init");
         setIsReady(true);
@@ -91,64 +96,93 @@ function App() {
 
     // Use Blender to check the name and path
     // Initialize Bone
-    setHead(
-      modelRef.current?.children[0]?.children[0]?.children[0]?.children[0]
-        ?.children[0]?.children[0]?.children[0]?.children[0]
-    );
-    setSpine(
-      modelRef.current?.children[0]?.children[0]?.children[0]?.children[0]
-    );
-    setLeftArm(
-      modelRef.current?.children[0]?.children[0]?.children[0]?.children[0]
-        ?.children[0]?.children[0]?.children[1]?.children[0]
-    );
-    setLeftForeArm(
-      modelRef.current?.children[0]?.children[0]?.children[0]?.children[0]
-        ?.children[0]?.children[0]?.children[1]?.children[0]?.children[0]
-    );
-    setRightArm(
-      modelRef.current?.children[0]?.children[0]?.children[0]?.children[0]
-        ?.children[0]?.children[0]?.children[2]?.children[0]
-    );
-    setRightForeArm(
-      modelRef.current?.children[0]?.children[0]?.children[0]?.children[0]
-        ?.children[0]?.children[0]?.children[2]?.children[0]?.children[0]
-    );
-    setLeftUpLeg(
-      modelRef.current?.children[0]?.children[0]?.children[0]?.children[1]
-    );
-    setLeftLeg(
-      modelRef.current?.children[0]?.children[0]?.children[0]?.children[1]
-        ?.children[0]
-    );
-    setRightUpLeg(
-      modelRef.current?.children[0]?.children[0]?.children[0]?.children[2]
-    );
-    setRightLeg(
-      modelRef.current?.children[0]?.children[0]?.children[0]?.children[2]
-        ?.children[0]
-    );
+    // setHead(
+    //   modelRef.current?.children[0]?.children[0]?.children[0]?.children[0]
+    //     ?.children[0]?.children[0]?.children[0]?.children[0]
+    // );
+    // setSpine(
+    //   modelRef.current?.children[0]?.children[0]?.children[0]?.children[0]
+    // );
+    // setLeftArm(
+    //   modelRef.current?.children[0]?.children[0]?.children[0]?.children[0]
+    //     ?.children[0]?.children[0]?.children[1]?.children[0]
+    // );
+    // setLeftForeArm(
+    //   modelRef.current?.children[0]?.children[0]?.children[0]?.children[0]
+    //     ?.children[0]?.children[0]?.children[1]?.children[0]?.children[0]
+    // );
+    // setRightArm(
+    //   modelRef.current?.children[0]?.children[0]?.children[0]?.children[0]
+    //     ?.children[0]?.children[0]?.children[2]?.children[0]
+    // );
+    // setRightForeArm(
+    //   modelRef.current?.children[0]?.children[0]?.children[0]?.children[0]
+    //     ?.children[0]?.children[0]?.children[2]?.children[0]?.children[0]
+    // );
+    // setLeftUpLeg(
+    //   modelRef.current?.children[0]?.children[0]?.children[0]?.children[1]
+    // );
+    // setLeftLeg(
+    //   modelRef.current?.children[0]?.children[0]?.children[0]?.children[1]
+    //     ?.children[0]
+    // );
+    // setRightUpLeg(
+    //   modelRef.current?.children[0]?.children[0]?.children[0]?.children[2]
+    // );
+    // setRightLeg(
+    //   modelRef.current?.children[0]?.children[0]?.children[0]?.children[2]
+    //     ?.children[0]
+    // );
 
     socketRef.current = startWebSocket();
     // socket.current = new WebSocket("wss://testsocket.danceoftaihou.live");
 
     socketRef.current.onmessage = (data) => {
       const json = JSON.parse(data.data);
-      // console.log(json);
       setData(json);
-      // console.log(leftForeArm)
 
-      // // rotation_movement(head, json);
-      // // rotation_movement(spine, json);
-      // // rotation_movement(hips, json);
-      // // rotation_movement(leftArm, json);
-      rotation_movement(leftForeArm, json);
-      // rotation_movement(rightArm, json);
-      // // rotation_movement(rightForeArm, json);
-      // // rotation_movement(leftLeg, json);
-      // // rotation_movement(leftUpLeg, json);
-      // // rotation_movement(rightLeg, json);
-      // // rotation_movement(rightUpLeg, json);
+      rotation_movement(rightArm, json)
+
+      // Rotationing
+      console.log(json.source);
+      switch (json.source) {
+        case "head":
+          rotation_movement(head, json)
+          break;
+        case "spine":
+          break;
+        case "rightArm":
+          rotation_movement(rightArm, json)
+          rotation_movement(rightForeArm, json)
+          break;
+        case "rightForeArm":
+          rotation_movement(rightForeArm,json)
+          break;
+        case "leftArm":
+          rotation_movement(leftArm, json)
+          rotation_movement(leftForeArm, json)
+          break;
+        case "leftForeArm":
+          rotation_movement(leftForeArm, json)
+          break;
+        case "rightUpLeg":
+          rotation_movement(rightUpLeg, json)
+          rotation_movement(rightLeg, json)
+          break;
+        case "rightLeg":
+          rotation_movement(rightLeg, json)
+          break;
+        case "leftUpLeg":
+          rotation_movement(leftUpLeg,json)
+          rotation_movement(leftLeg,json)
+          break;
+        case "leftLeg":
+          rotation_movement(leftLeg,json)
+          break;
+      }
+
+      // Positioning
+      position_movement(body, json)
     };
 
     return () => {
@@ -188,10 +222,13 @@ function App() {
   }
 
   // Part Position Movement
-  function position_movement(part, px, py, pz) {
-    part.position.x = px;
-    part.position.y = py;
-    part.position.z = pz;
+  function position_movement(part, data) {
+    // part.position.x = data.x;
+    // part.position.y = data.y;
+    // part.position.z = data.z;
+    part.position.x = data.rot_x;
+    // part.position.y = data.rot_y;
+    part.position.z = data.rot_z;
   }
 
   // Convert Degree to Radian
@@ -200,18 +237,19 @@ function App() {
     return radian;
   }
 
-  const modelList = ["female", "male"];
+  // const modelList = ["male", "female"];
 
-  async function changeAvatar() {
-    setIsReady(false);
-    setIndex((index + 1) % modelList.length);
-    model.removeAttribute("gltf-model");
-    model.setAttribute("gltf-model", `#${modelList[index]}`);
-    let newModel = document.getElementById("model").object3D;
-    modelRef.current = newModel;
-    await delay(1000);
-    setIsReady(true);
-  }
+  // async function changeAvatar() {
+  //   setIsReady(false);
+  //   setIndex((index + 1) % modelList.length);
+  //   console.log(modelRef.current);
+  //   modelRef.current.removeAttribute("gltf-model");
+  //   modelRef.current.setAttribute("gltf-model", `#${modelList[index]}`);
+  //   // let newModel = document.getElementById("model").object3D;
+  //   // modelRef.current = newModel;
+  //   await delay(1000);
+  //   setIsReady(true);
+  // }
 
   const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -258,7 +296,7 @@ function App() {
         <div className="indicator">X = {data.rot_x}</div>
         <div className="indicator">Y = {data.rot_y}</div>
         <div className="indicator">Z = {data.rot_z}</div>
-        <button onClick={changeAvatar}>click me</button>
+        {/* <button onClick={changeAvatar}>click me</button> */}
       </div>
     </div>
   );
