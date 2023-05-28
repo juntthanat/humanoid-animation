4e164bb86459f7fcb8f6c9fce18aabbf239f4668
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2023-05-28 17:17:42 +0700

    remove: page

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./Page/App";
-import Login from "./Page/Login";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Login/>,
-  },
-  {
-    path: "/App",
-    element: <App />,
+    element: <App/>,
   },
 ]);
 
